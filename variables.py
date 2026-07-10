9e6edfb228ad00eb05e70119cb661cf4e7f3126c
pratyushpalx18 <pratyush26mtce@student.pillai.edu.in>
2026-07-10 18:21:20 +0530

    Added variables.py

---
+++ variables.py
@@ -0,0 +1,23 @@
+x=10
+print(x)
+print(type(x))
+a=20
+b=30
+c=a+b
+print(c)
+print(type(c))
+
+name=input("Enter your Name: ")
+age=int(input("Enter your Age: "))
+branch=input("Enter ypur Branch in B.Tech: ")
+city=input("Enter your City: ")
+
+print("Name:",name)
+print("Age:",age)
+print("Branch:",branch)
+print("City:",city)
+
+print(type(name))
+print(type(age))
+print(type(branch))
+print(type(city))
